5feb8ceac0a72fd6c814a5111ad1c284aeb998f3
ubmt2 <alexdemin1607@gmail.com>
2026-06-03 23:03:54 +0300

    Добавлен класс Recipe

--- OOP.py
+++ OOP.py
@@ -1,9 +1,11 @@
+from typing import List
+
+
 class Ingredient:
     def __init__(self, name: str, quantity: float, unit: str) -> None:
         self.name = name
         self.quantity = quantity
         self.unit = unit
-
 
     @property
     def quantity(self) -> float:
@@ -28,3 +30,38 @@ class Ingredient:
             return True
 
         return False
+
+
+class Recipe:
+    def __init__(self, title: str, ingredients: List[Ingredient]) -> None:
+        self.title = title
+        self.ingredients = ingredients
+
+    def add_ingredient(self, ingredient: Ingredient) -> None:
+        if ingredient in self.ingredients:
+            self.ingredients[self.ingredients.index(ingredient)].quantity += ingredient.quantity
+        else:
+            self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio: int | float) -> bool:
+        return ratio > 0
+
+    def scale(self, ratio: float):
+        output = Recipe(self.title, [])
+        for i in self.ingredients:
+            i.quantity *= ratio
+            output.add_ingredient(i)
+
+        return output
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        output = f"{self.title}:\n"
+
+        for i in self.ingredients:
+            output += str(i) + "\n"
+
+        return output
